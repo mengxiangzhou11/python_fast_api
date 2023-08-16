c50a411e7c718d8658023b5bc3fc3998926579b4
mxz11694 <!@#123qwe>
2023-08-16 15:52:04 +0800

    fix-dxpool登录

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from typing import Union
 
 from fastapi import FastAPI
 import requests
+import json
 
 app = FastAPI()
 
@@ -15,6 +16,21 @@ def read_root():
     url = 'https://www.f2pool.com/miners'
     data = {}
     r = requests.post(url, data=data)
+    return {"status": 200,"msg": r.text}
+
+#请求鱼池矿机数据
+@app.get("/lbc")
+def read_root():
+    data = {
+        'account': '86.15370406510',
+        'password': 'a123456789'
+    }
+
+    ## headers中添加上content-type这个参数，指定为json格式
+    headers = {'Content-Type': 'application/json'}
+
+    ## post的时候，将data字典形式的参数用json包转换成json格式。
+    r = requests.post(url='https://www.dxpool.com/api/v2/users/login', headers=headers, data=json.dumps(data))
     return {"status": 200,"msg": r.text}
 
 
