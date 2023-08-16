61a7edddfd56370e2e60c73316985562687edc07
mxz11694 <!@#123qwe>
2023-08-16 17:56:48 +0800

    fix-token 写死

--- main.py
+++ main.py
@@ -23,16 +23,16 @@ def read_root():
 def read_root():
 
     ##########################################登录#########################################################
-    data = {
-        'account': '86.15370406510',
-        'password': 'a123456789'
-    }
-
-    headers = {'Content-Type': 'application/json'}
-
-    res = requests.post(url='https://www.dxpool.com/api/v2/users/login', headers=headers, data=json.dumps(data))
-    res_value = json.loads(res.text)
-    token = res_value['token']
+    # data = {
+    #     'account': '86.15370406510',
+    #     'password': 'a123456789'
+    # }
+    #
+    # headers = {'Content-Type': 'application/json'}
+    #
+    # res = requests.post(url='https://www.dxpool.com/api/v2/users/login', headers=headers, data=json.dumps(data))
+    # res_value = json.loads(res.text)
+    # token = res_value['token']
 
     try:
         ##########################################请求lbc旷工列表#########################################################
@@ -43,7 +43,7 @@ def read_root():
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36',
             'Content-Type': 'application/json',
             # 'Cookie': '_ga_XTG5DEQWQM=deleted; _ga_3BTDE2KT80=GS1.2.1690421988.1.0.1690421988.0.0.0; _ga=GA1.2.1678147956.1686722153; _gid=GA1.2.1875331741.1692170634; userID=5771; _csrf=94dc5551DgPePAE5zPYmtHaq2UN2x5prCK9UZveob7496e29466caf452dedc0f3a%3A2%3A%7Bi%3A0%3Bs%3A5%3A%22_csrf%22%3Bi%3A1%3Bs%3A32%3A%224OCQDAPjw8XshB6iNsm8PUIvYERFfWnM%22%3B%7D; _gat_UA-128180639-2=1; _ga_XTG5DEQWQM=GS1.1.1692170633.19.1.1692172003.0.0.0',
-            'authorization': 'Bearer ' + token,
+            'authorization': 'Bearer ' + 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2OTDPe21gqq1Sd7E1c6sCZsgrTu9TqurWF5v3fQ.f9ioXl2_c-rWcvCgQ0iQtH1_paEhozgHYKCXjQmil9w',
         }
 
         res = requests.get(
